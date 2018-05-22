2bbbd64c441680da089339f1f5993912ee540dd3
alex <38940961+kintdev@users.noreply.github.com>
2018-05-22 13:57:25 +0200

    Add files via upload

---
+++ Foodscript.ps1
@@ -0,0 +1,15 @@
+﻿# Food selector for the week!
+#random Stuff mixed for every day.
+function Random-Food{
+
+Param(
+     [array]$InputList
+   )
+
+   return $InputList | Get-Random -Count 7 
+   $InputList.Count;
+}
+
+$a = 1..200
+Write-Output (Random-Food -InputList $a)
+
